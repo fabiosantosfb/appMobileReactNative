67f7722b828d4f152f1d0817903a2087cc76f0d7
fabiano <fabiodeveloperti@gmail.com>
2018-08-26 18:01:10 -0300

    inicio

---
+++ README.md
@@ -0,0 +1,26 @@
+Android Mobile React Native
+=========
+### Autenticação utilizando api facebook
+
+`Utilidade` - app simples para teste de funcionalidade de autenticação com api do facebook!
+
+#### Funcionalidades:
+
+ * Autenticação
+ * Tela Com sessões de do usuario autenticado
+
+
+Instalação
+-----
+
+Instalar `npm`:
+
+``` bash
+$ npm install
+```
+
+License
+-------
+
+(The MIT License)
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
+            </View>
+        );
+    }
+}
